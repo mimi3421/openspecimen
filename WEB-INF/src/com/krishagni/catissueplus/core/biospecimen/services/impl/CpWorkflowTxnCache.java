8d8e34e78e4ddc5b61a512b7d4861fb73e70c343
Vinayak Pawar <vinayakapawar@gmail.com>
2018-01-23 14:02:59 +0530

    Implemented transactional cache for CP workflows. This can be used to prevent fetching same CP workflow multiple times, once for each specimen, from DB in the same transaction.

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/CpWorkflowTxnCache.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/CpWorkflowTxnCache.java
@@ -1,4 +1,44 @@
 package com.krishagni.catissueplus.core.biospecimen.services.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import com.krishagni.catissueplus.core.biospecimen.domain.CpWorkflowConfig;
+import com.krishagni.catissueplus.core.biospecimen.repository.DaoFactory;
+import com.krishagni.catissueplus.core.common.TransactionalThreadLocals;
+
+@Configurable
 public class CpWorkflowTxnCache {
+	private final static CpWorkflowTxnCache instance = new CpWorkflowTxnCache();
+
+	private ThreadLocal<Map<Long, CpWorkflowConfig>> cpWorkflows = new ThreadLocal<Map<Long, CpWorkflowConfig>>() {
+		@Override
+		protected Map<Long, CpWorkflowConfig> initialValue() {
+			TransactionalThreadLocals.getInstance().register(this);
+			return new HashMap<>();
+		}
+	};
+
+	@Autowired
+	private DaoFactory daoFactory;
+
+	public static CpWorkflowTxnCache getInstance() {
+		return instance;
+	}
+
+	public CpWorkflowConfig.Workflow getWorkflow(Long cpId, String name) {
+		CpWorkflowConfig config = cpWorkflows.get().get(cpId);
+		if (config == null) {
+			config = daoFactory.getCollectionProtocolDao().getCpWorkflows(cpId);
+			if (config == null) {
+				config = new CpWorkflowConfig();
+				cpWorkflows.get().put(cpId, config);
+			}
+		}
+
+		return config.getWorkflows().get(name);
+	}
 }
