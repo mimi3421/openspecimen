a63e295ea337c541a8a17c28eea6e083b1fc9d87
Vinayak Pawar <vinayakapawar@gmail.com>
2018-01-26 19:11:14 +0530

    Simplified the expression parsing. Got rid of rewriting of expressions ...

--- WEB-INF/src/com/krishagni/catissueplus/core/common/util/ExpressionUtil.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/util/ExpressionUtil.java
@@ -1,20 +1,12 @@
 package com.krishagni.catissueplus.core.common.util;
 
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.expression.spel.SpelNode;
-import org.springframework.expression.spel.ast.CompoundExpression;
 import org.springframework.expression.spel.standard.SpelExpression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class ExpressionUtil {
-	private static final Log logger = LogFactory.getLog(ExpressionUtil.class);
-
 	private static ExpressionUtil instance = new ExpressionUtil();
 
 	private SpelExpressionParser parser = new SpelExpressionParser();
@@ -33,29 +25,6 @@ public class ExpressionUtil {
 	}
 
 	private SpelExpression parse(String exprStr) {
-		SpelExpression expr = parser.parseRaw(exprStr);
-		Set<String> variables = getVariables(expr.getAST(), new HashSet<>());
-
-		String finalExpr = expr.toStringAST();
-		for (String var : variables) {
-			finalExpr = finalExpr.replaceAll(var + " ", "#" + var + " ");
-		}
-
-		if (logger.isDebugEnabled()) {
-			logger.debug(String.format("The expression %s is rewritten as %s", exprStr, finalExpr));
-		}
-		return parser.parseRaw(finalExpr);
-	}
-
-	private Set<String> getVariables(SpelNode node, Set<String> result) {
-		if (node instanceof CompoundExpression) {
-			result.add(node.toStringAST());
-		}
-
-		for (int i = 0; i < node.getChildCount(); ++i) {
-			getVariables(node.getChild(i), result);
-		}
-
-		return result;
+		return parser.parseRaw(exprStr);
 	}
 }
