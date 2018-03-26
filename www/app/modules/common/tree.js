201baebdb96bf78e3e2ddbe28e3123295ecd52d7
Vinayak Pawar <vinayakapawar@gmail.com>
2018-03-26 10:55:34 +0530

    OPSMN-4212: Used ng-click instead of ng-change to notify the change in checkbox status.
    The intermediate tree nodes do not have the model values (true/false) set. Their
    checkboxes are ticked based on whether any of their descendant nodes are ticked or not.
    As a result, when ticked nodes are clicked for first time, their model value is set to
    false. This will not trigger ng-change handler and as a result there will be no change
    in checkbox status of its descendants.
    
    Use of ng-click will invoke the handler everytime the checkbox is clicked irrespective
    of its state.

--- www/app/modules/common/tree.js
+++ www/app/modules/common/tree.js
@@ -94,7 +94,7 @@ angular.module('openspecimen')
         '    </div> ' +
         '    <div class="os-tree-node-checkbox"> ' +
         '      <input type="checkbox" ng-model="node.checked" ng-checked="isNodeChecked(node)" ' +
-        '        ng-change="nodeChecked(node)"> ' +
+        '        ng-click="nodeChecked(node)"> ' +
         '    </div> ' +
         '    <div class="os-tree-node-label"> ' +
         '      <div ng-if="nodeTmpl" ng-include src="nodeTmpl"></div> ' +
