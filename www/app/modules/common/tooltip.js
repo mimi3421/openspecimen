fcc8359ff19d00e07e7dd5fff3db534f48bb3635
Vinayak Pawar <vinayakapawar@gmail.com>
2018-05-25 12:38:16 +0530

    OPSMN-4354: Implementation of os-tooltip widget.
    There was no tooltip widget in OS. 90% of the app views were dependent
    on tooltips rendered by the browser. However, the presentation of tooltip
    varied across browsers. For example, Safari renders the tooltip after
    lot of delay, in very small fonts making them practically unusable.
    
    In order to unify the tooltip presentation and make them attractive
    and useful, implemented a custom tooltip widget. The usage of this
    widget is given below:
    
    <some-element os-tooltip="Hardcoded tooltip string">
      <!-- something more here -->
    </some-element>
    
    <some-element os-tooltip="{{'i18n_key' | translate}}">
      <!-- something more here -->
    </some-element>

--- www/app/modules/common/tooltip.js
+++ www/app/modules/common/tooltip.js
@@ -26,4 +26,35 @@ angular.module('openspecimen')
         );
       }
     };
+  })
+
+  .directive('osTooltip', function($compile) {
+    return {
+      restrict: 'A',
+
+      terminal: true,
+
+      priority: 100000,
+
+      compile: function(tElem, tAttrs) {
+        var tooltip = tAttrs.osTooltip;
+        tElem.removeAttr('os-tooltip');
+        tElem.attr({
+          'bs-tooltip': '',
+          'data-title': tooltip,
+          'trigger'   : 'hover focus',
+          'placement' : 'auto'
+        });
+
+        var linkFn = $compile(tElem);
+        return {
+          pre: function(scope, element, attrs) {
+          },
+
+          post: function(scope, element, attrs) {
+            linkFn(scope, function(clone) { element.replaceWith(clone); });
+          }
+        }
+      }
+    }
   });
