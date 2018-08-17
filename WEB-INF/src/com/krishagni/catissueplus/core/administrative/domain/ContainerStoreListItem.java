cedddf1a851a240f14086775de895b0d656d2359
Vinayak Pawar <vinayakapawar@gmail.com>
2018-08-17 09:05:16 +0530

    Default the store list item put/pick ack time to current time. Not all automated freezer APIs will respond with an ack.

--- WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/ContainerStoreListItem.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/ContainerStoreListItem.java
@@ -1,5 +1,6 @@
 package com.krishagni.catissueplus.core.administrative.domain;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import com.krishagni.catissueplus.core.biospecimen.domain.BaseEntity;
@@ -18,7 +19,7 @@ public class ContainerStoreListItem extends BaseEntity {
 
 	private Status status = Status.PENDING;
 
-	private Date ackTime;
+	private Date ackTime = Calendar.getInstance().getTime();
 
 	private String error;
 
