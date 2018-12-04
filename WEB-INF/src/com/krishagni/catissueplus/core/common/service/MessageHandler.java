3afad8cab85e24bb57a6b7ed9cb4669da01151ff
Vinayak Pawar <vinayakapawar@gmail.com>
2018-12-04 18:01:50 +0530

    1. Added onStart and onComplete callbacks in the external app message    handler interface. These callbacks are invoked by the retry thread    during init and teardown phase of the retry process. The callbacks    allow external app message handlers to initialise and clean up the    data structures needed for message processing.
       For example - REDCap message processing requires dictionary.
       Building and destroying dictionary for every single message will be
       a costly affair. Therefore the callbacks allow the message processor
       to know when the dictionary should be built and cleaned up.
    
    2. Store the latest error message encountered during retry in the
       messages log table.

--- WEB-INF/src/com/krishagni/catissueplus/core/common/service/MessageHandler.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/service/MessageHandler.java
@@ -3,5 +3,9 @@ package com.krishagni.catissueplus.core.common.service;
 import com.krishagni.catissueplus.core.common.domain.MessageLog;
 
 public interface MessageHandler {
+	void onStart();
+
 	String process(MessageLog log);
+
+	void onComplete();
 }
--- WEB-INF/src/com/krishagni/catissueplus/core/common/service/impl/MessageLogServiceImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/service/impl/MessageLogServiceImpl.java
@@ -16,6 +16,7 @@ import com.krishagni.catissueplus.core.common.events.MessageLogCriteria;
 import com.krishagni.catissueplus.core.common.service.MessageHandler;
 import com.krishagni.catissueplus.core.common.service.MessageLogService;
 import com.krishagni.catissueplus.core.common.util.ConfigUtil;
+import com.krishagni.catissueplus.core.common.util.Utility;
 
 public class MessageLogServiceImpl implements MessageLogService {
 	private static final Log logger = LogFactory.getLog(MessageLogServiceImpl.class);
@@ -35,38 +36,54 @@ public class MessageLogServiceImpl implements MessageLogService {
 
 	@Override
 	public void retryPendingMessages() {
-		MessageLogCriteria crit = new MessageLogCriteria()
-			.processStatus(MessageLog.ProcessStatus.PENDING)
-			.toDate(Calendar.getInstance().getTime())
-			.maxRetries(getMaxRetries());
+		try {
+			started();
 
-		int startAt = 0, maxLogs = 50;
-		boolean endOfMsgLogs = false;
-		while (!endOfMsgLogs) {
-			List<MessageLog> msgLogs = getLogs(crit.startAt(startAt).maxResults(maxLogs));
-			endOfMsgLogs = (msgLogs.size() < maxLogs);
+			MessageLogCriteria crit = new MessageLogCriteria()
+				.processStatus(MessageLog.ProcessStatus.PENDING)
+				.toDate(Calendar.getInstance().getTime())
+				.maxRetries(getMaxRetries());
 
-			for (MessageLog msgLog : msgLogs) {
-				String recordIds = null;
-				try {
-					MessageHandler handler = handlers.get(msgLog.getExternalApp());
-					if (handler == null) {
-						logger.error("No handler registered to process messages of external app: " + msgLog.getExternalApp());
-						continue;
+			int startAt = 0, maxLogs = 50;
+			boolean endOfMsgLogs = false;
+			while (!endOfMsgLogs) {
+				List<MessageLog> msgLogs = getLogs(crit.startAt(startAt).maxResults(maxLogs));
+				endOfMsgLogs = (msgLogs.size() < maxLogs);
+
+				for (MessageLog msgLog : msgLogs) {
+					String error = null;
+					String recordIds = null;
+					try {
+						MessageHandler handler = handlers.get(msgLog.getExternalApp());
+						if (handler == null) {
+							logger.error("No handler registered to process messages of external app: " + msgLog.getExternalApp());
+							continue;
+						}
+
+						recordIds = handler.process(msgLog);
+					} catch (Exception e) {
+						logger.error("Error while processing message with log id = " + msgLog.getId(), e);
+						error = Utility.getErrorMessage(e);
+					} finally {
+						updateStatus(msgLog, recordIds, error);
 					}
-
-					recordIds = handler.process(msgLog);
-				} catch (Exception e) {
-					logger.error("Error while processing message with log id = " + msgLog.getId(), e);
-				} finally {
-					updateStatus(msgLog, recordIds);
 				}
 			}
+		} finally {
+			finished();
 		}
 	}
 
 	private int getMaxRetries() {
 		return ConfigUtil.getInstance().getIntSetting("common", "max_eapp_msg_retries", 5);
+	}
+
+	private void started() {
+		handlers.values().forEach(MessageHandler::onStart);
+	}
+
+	private void finished() {
+		handlers.values().forEach(MessageHandler::onComplete);
 	}
 
 	@PlusTransactional
@@ -75,10 +92,12 @@ public class MessageLogServiceImpl implements MessageLogService {
 	}
 
 	@PlusTransactional
-	private void updateStatus(MessageLog msgLog, String recordIds) {
-		if (StringUtils.isNotBlank(recordIds)) {
+	private void updateStatus(MessageLog msgLog, String recordIds, String error) {
+		if (StringUtils.isNotBlank(recordIds) && StringUtils.isBlank(error)) {
 			msgLog.setRecordId(recordIds);
 			msgLog.setProcessStatus(MessageLog.ProcessStatus.PROCESSED);
+		} else if (StringUtils.isNotBlank(error)) {
+			msgLog.setError(error);
 		}
 
 		msgLog.incrNoOfRetries();
