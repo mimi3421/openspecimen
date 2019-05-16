e75e6791245b580e1cd9e584092047225ea31bf0
Vinayak Pawar <vinayakapawar@gmail.com>
2019-05-16 13:10:50 +0530

    OPSMN-4928: Do not prefix http:// when the tracking URL starts with either http:// or https://

--- www/app/modules/common/http-prefixer.js
+++ www/app/modules/common/http-prefixer.js
@@ -1,7 +1,7 @@
 angular.module('openspecimen')
   .filter('osHttpPrefixer', function() {
     return function(input) {
-      if (!input || input.indexOf("http://") == 0) {
+      if (!input || input.indexOf('http://') == 0 || input.indexOf('https://') == 0) {
         return input;
       }
 
