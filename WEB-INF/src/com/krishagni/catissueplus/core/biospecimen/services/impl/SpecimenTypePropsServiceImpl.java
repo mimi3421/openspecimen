fd26e8c5fec2638e7011ae4bd17769a03d609b62
Vinayak Pawar <vinayakapawar@gmail.com>
2019-06-13 18:38:50 +0530

    Fixing v6.1.x branch with the following missing commits
    6972fe2c964d7bbc32abcb2830b3618f371440b7
    OPSMN-4939: Fixed the API to load properties of all the configured types.
    
    be7270aeedec60bf980b9cd4d6c3434851ec488f
    Fixed the PV seed data files to be compatible with the latest version of Liquibase.
    
    Removed null valued columns from the PV data files.

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/SpecimenTypePropsServiceImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/SpecimenTypePropsServiceImpl.java
@@ -6,11 +6,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
 
 import com.krishagni.catissueplus.core.administrative.domain.PermissibleValue;
-import com.krishagni.catissueplus.core.administrative.events.ListPvCriteria;
 import com.krishagni.catissueplus.core.biospecimen.events.SpecimenTypeProps;
-import com.krishagni.catissueplus.core.biospecimen.repository.DaoFactory;
 import com.krishagni.catissueplus.core.biospecimen.services.SpecimenTypePropsService;
 import com.krishagni.catissueplus.core.common.PlusTransactional;
 import com.krishagni.catissueplus.core.common.PvAttributes;
@@ -19,23 +21,29 @@ import com.krishagni.catissueplus.core.common.events.ResponseEvent;
 
 public class SpecimenTypePropsServiceImpl implements SpecimenTypePropsService {
 	
-	private DaoFactory daoFactory;
+	private SessionFactory sessionFactory;
 	
-	public void setDaoFactory(DaoFactory daoFactory) {
-		this.daoFactory = daoFactory;
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	@PlusTransactional
 	public ResponseEvent<List<SpecimenTypeProps>> getProps() {
 		try {
-			ListPvCriteria crit = new ListPvCriteria().attribute(PvAttributes.SPECIMEN_CLASS);
-			List<PermissibleValue> specimenClasses = daoFactory.getPermissibleValueDao().getPvs(crit);
+			List<PermissibleValue> specimenClasses = sessionFactory.getCurrentSession()
+				.createCriteria(PermissibleValue.class, "pv")
+				.createAlias("pv.parent", "ppv", JoinType.LEFT_OUTER_JOIN)
+				.add(Restrictions.eq("pv.attribute", PvAttributes.SPECIMEN_CLASS))
+				.add(Restrictions.isNull("ppv.id"))
+				.addOrder(Order.asc("pv.value"))
+				.list();
 
 			List<SpecimenTypeProps> props = new ArrayList<>();
 			for (PermissibleValue specimenClass : specimenClasses) {
 				for (PermissibleValue type : specimenClass.getChildren()) {
-					props.add(getSpecimenTypeProps(type));
+					props.add(getTypeProps(type));
 				}
 			}
 
@@ -47,7 +55,7 @@ public class SpecimenTypePropsServiceImpl implements SpecimenTypePropsService {
 		}
 	}
 
-	private SpecimenTypeProps getSpecimenTypeProps(PermissibleValue pv) {
+	private SpecimenTypeProps getTypeProps(PermissibleValue pv) {
 		Map<String, String> props = new HashMap<>();
 		props.put("abbreviation",        getProperty(pv, "abbreviation"));
 		props.put("qtyUnit",             getProperty(pv, "quantity_unit"));
