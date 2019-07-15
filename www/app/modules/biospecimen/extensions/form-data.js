309ba57cb192680b790aff8d3d6aafa8e8a02f93
Vinayak Pawar <vinayakapawar@gmail.com>
2019-07-15 17:51:09 +0530

    OPSMN-4985: Fixed the form data overview widget to automatically choose the label/value alignment based on the number of fields with longer labels.
    1. The label/value alignment is vertical if the form has more than 30% fields
       whose labels are greater than or equals 30 characters in length.
    
    2. Otherwise the label/value alignment defaults to horizontal, the present setting.
    
    3. Fixed the widget to display label/value in both vertical and horizontal layout.

--- www/app/modules/biospecimen/extensions/form-data.js
+++ www/app/modules/biospecimen/extensions/form-data.js
@@ -12,6 +12,23 @@ angular.module('os.biospecimen.extensions')
 
       link: function(scope, element, attrs) {
         scope.filesUrl = ApiUrls.getBaseUrl() + 'form-files';
+
+        var longerCaptionFields  = 0, totalFields = 0;
+        angular.forEach(scope.data.fields,
+          function(field) {
+            if (field.type == 'subForm') {
+              return;
+            }
+
+            if (field.caption && field.caption.length >= 30) {
+              ++longerCaptionFields;
+            }
+
+            ++totalFields;
+          }
+        );
+
+        scope.verticalLayout = (longerCaptionFields * 100 / totalFields >= 30);
       }
     };
   });
