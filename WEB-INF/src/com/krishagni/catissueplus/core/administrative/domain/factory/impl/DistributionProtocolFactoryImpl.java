8fa511dd1730865753dc13673269e6163f61ac0d
Vinayak Pawar <vinayakapawar@gmail.com>
2020-11-06 18:30:58 +0530

    OPSMN-5341: Fixed the distribution protocol create/update API, which was crashing when an invalid receiving institute was specified.

--- WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/factory/impl/DistributionProtocolFactoryImpl.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/administrative/domain/factory/impl/DistributionProtocolFactoryImpl.java
@@ -163,7 +163,7 @@ public class DistributionProtocolFactoryImpl implements DistributionProtocolFact
 			return;
 		}
 		
-		if (!defReceivingSite.getInstitute().equals(dp.getInstitute())) {
+		if (dp.getInstitute() != null && !defReceivingSite.getInstitute().equals(dp.getInstitute())) {
 			ose.addError(SiteErrorCode.INVALID_SITE_INSTITUTE, defReceivingSite.getName(), dp.getInstitute().getName());
 			return;
 		}
